de702b38704a01968416ed82f1672dea38e545c1
osadeyemo <osadeyemo@yahoo.com>
2023-02-23 11:20:06 +0000

    constructors lab completed

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -48,7 +48,7 @@ public class Player {
      * @param level the starting level of the player to be created.
      */
     public Player(String playerName, int level){
-
+        this.playerName = playerName;
+        this.level = level;
     }
-
 }
