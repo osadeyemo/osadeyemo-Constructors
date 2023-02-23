042fa734610872fc5278a81032fc8eed69546750
osadeyemo <osadeyemo@yahoo.com>
2023-02-23 11:36:48 +0000

    Constructor lab updated

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -31,7 +31,8 @@ public class Player {
      * @param playerName the name of the player to be created.
      */
     public Player(String playerName){
-
+        this.playerName = playerName;
+        this.level = 1;
     }
 
     /**
